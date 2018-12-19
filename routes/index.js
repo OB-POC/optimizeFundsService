8151ce088901dd4844764d795a439cbae0da075b
nitinmaram <nitinmaram@gmail.com>
2018-12-19 17:07:47 +0530

    fixed issue of balances

--- routes/index.js
+++ routes/index.js
@@ -5,84 +5,6 @@ var config = require('../data/config');
 var serviceUrlConfig  = require("../data/serviceURL's");
 var request = require('request');
 
-/* GET home page. */
-router.get('/si/suggestions', function(req, res, next) {
-  var token = req.headers['x-access-token'];
-  jwt.verify(token, config.secret , function(err, decodedObj){
-    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-    var userName = decodedObj.username;
-    request.get(serviceUrlConfig.dbUrl+'/'+userName+'-debit', function(err, response, body){
-      if(err) return res.status(500).json({ message: 'Failed to load data'})
-      // console.log(body);
-      var accountsArray = JSON.parse(body)['banks'].map((bank)=>{
-        bank.accounts[0].bankName = bank.bankName;
-        return bank.accounts[0]
-      })
-      var sortedArray = accountsArray.sort((a,b)=>{
-        return a.interestRate-b.interestRate;
-      });
-      console.log(sortedArray);
-      res.status(200).json({
-        senders: [
-          {
-            senderBank: sortedArray[0].bankName,
-            senderAccountNumber: sortedArray[0].accountNumber,
-            senderAer: sortedArray[0].interestRate,
-            amount: sortedArray[1] && Math.abs(parseInt(sortedArray[1].balance) - parseInt(sortedArray[1].minBalance) - parseInt(sortedArray[1].standingInst))
-          }
-        ],
-        receiver: {
-          receiverBank: sortedArray[1] && sortedArray[1].bankName,
-          receiverAccountNumber: sortedArray[1] && sortedArray[1].accountNumber,
-          receiverAer: sortedArray[1] && sortedArray[1].interestRate
-        }
-      })
-    })
-  });
-});
-
-
-router.post('/si/suggestions', function(req, res, next) {
-  var token = req.headers['x-access-token'];
-  var postData = req.body;
-  jwt.verify(token, config.secret , function(err, decodedObj){
-    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-    var userName = decodedObj.username;
-    request.get(serviceUrlConfig.dbUrl+'/'+userName+'-debit', function(err, response, body){
-      if(err) return res.status(500).json({ message: 'Failed to load data'})
-      // console.log(body, postData.transfers);
-      var data = JSON.parse(body);
-      postData.senders.map((obj)=>{
-        var filteredSenderBank = data.banks.filter((bank)=>{
-          return bank.bankName == obj.senderBank;
-        })[0];
-        var filteredReceiverBank = data.banks.filter((bank)=>{
-          return bank.bankName == postData.receiver.receiverBank
-        })[0];
-        var restBankDetails = data.banks.filter((bank)=>{
-          return bank.bankName != postData.receiver.receiverBank && bank.bankName != obj.senderBank;
-        });
-        filteredSenderBank.accounts[0].balance = parseInt(filteredSenderBank.accounts[0].balance) - parseInt(obj.amount);
-        filteredReceiverBank.accounts[0].balance = parseInt(filteredReceiverBank.accounts[0].balance) + parseInt(obj.amount);
-        filteredReceiverBank.accounts[0].availableBalance = 0
-        filteredReceiverBank.accounts[0].standingInstructions.map((obj)=>obj.canClear=true);
-        data.banks = [...restBankDetails, filteredReceiverBank, filteredSenderBank];
-      });
-      request.patch({
-        url: serviceUrlConfig.dbUrl+'/'+userName+'-debit',
-        body: {
-          'banks': data.banks
-        },
-        json: true
-      }, function(err, response, body){
-        if(err) return res.status(500).json({ message: 'Failed to patch data'})
-        console.log(body);
-        res.status(200).json(body);
-      })
-    })
-  });
-})
-
 router.post('/mergeFunds', function(req, res, next) {
   var token = req.headers['x-access-token'];
   var postData = req.body;
@@ -103,7 +25,7 @@ router.post('/mergeFunds', function(req, res, next) {
         var restBankDetails = data.banks.filter((bank)=>{
           return bank.bankName != postData.receiver.receiverBank && bank.bankName != obj.senderBank;
         });
-        filteredSenderBank.accounts[0].balance = parseInt(filteredSenderBank.accounts[0].balance) - parseInt(filteredSenderBank.accounts[0].availableBalance);
+        filteredSenderBank.accounts[0].balance = parseInt(filteredSenderBank.accounts[0].balance) - parseInt(filteredSenderBank.accounts[0].minBalance) - parseInt(filteredSenderBank.accounts[0].standingInst) ;
         filteredReceiverBank.accounts[0].balance = parseInt(filteredReceiverBank.accounts[0].balance) + parseInt(filteredSenderBank.accounts[0].availableBalance);
         filteredReceiverBank.accounts[0].availableBalance += parseInt(filteredSenderBank.accounts[0].availableBalance);
         filteredSenderBank.accounts[0].availableBalance = 0;
@@ -123,66 +45,4 @@ router.post('/mergeFunds', function(req, res, next) {
     })
   });
 })
-
-router.post('/mergeAccounts', function(req, res, next) {
-  var token = req.headers['x-access-token'];
-  var postData = req.body;
-  jwt.verify(token, config.secret , function(err, decodedObj){
-    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-    var userName = decodedObj.username;
-    request.get(serviceUrlConfig.dbUrl+'/'+userName+'-debit', function(err, response, body){
-      if(err) return res.status(500).json({ message: 'Failed to load data'})
-      // console.log(body, postData.transfers);
-      var data = JSON.parse(body);
-      postData.senders.map((obj)=>{
-        var filteredSenderBank = data.banks.filter((bank)=>{
-          return bank.bankName == obj.senderBank;
-        })[0];
-        var filteredReceiverBank = data.banks.filter((bank)=>{
-          return bank.bankName == postData.receiver.receiverBank
-        })[0];
-        if(!filteredReceiverBank){
-          filteredReceiverBank = {
-            "bankName": "LBG",
-            "bankId": "LLBBGG",
-            "accounts": [
-              {
-                "accountType": "SB",
-                "accountNumber": "XXXXXX XXXX2222",
-                "accountTitle": "Easy Saver",
-                "standingInst": 0,
-                "balance": 0,
-                "minBalance": 200,
-                "interestRate": 0.5,
-                "availableBalance": 0,
-                "automatedSITransations": false,
-                "standingInstructions": []
-              }
-            ]
-          }
-        }
-        var restBankDetails = data.banks.filter((bank)=>{
-          return bank.bankName != postData.receiver.receiverBank && bank.bankName != obj.senderBank;
-        });
-        filteredReceiverBank.accounts[0].balance = parseInt(filteredReceiverBank.accounts[0].balance) + parseInt(filteredSenderBank.accounts[0].balance);
-        filteredReceiverBank.accounts[0].standingInstructions = [...filteredReceiverBank.accounts[0].standingInstructions, ...filteredSenderBank.accounts[0].standingInstructions];
-        filteredReceiverBank.accounts[0].standingInst = filteredReceiverBank.accounts[0].standingInstructions.reduce((acc, val)=>acc+val.value,0);
-        filteredSenderBank.accounts[0].availableBalance = filteredReceiverBank.accounts[0].balance - filteredReceiverBank.accounts[0].standingInstructions - filteredReceiverBank.accounts[0].standingInst;
-        data.banks = [...restBankDetails, filteredReceiverBank];
-      });
-      request.patch({
-        url: serviceUrlConfig.dbUrl+'/'+userName+'-debit',
-        body: {
-          'banks': data.banks
-        },
-        json: true
-      }, function(err, response, body){
-        if(err) return res.status(500).json({ message: 'Failed to patch data'})
-        console.log(body);
-        res.status(200).json(body);
-      })
-    })
-  });
-})
-
 module.exports = router;
